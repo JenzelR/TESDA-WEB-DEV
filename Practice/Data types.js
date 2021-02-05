a7f1adda5ea47e81b115aba11407c819ae4b7a65
Jenzel Carl D. Rivera <jenzcar7@gmail.com>
2021-02-06 01:23:52 +0800

    LO1 Assignment

---
+++ Practice/Data types.js
@@ -0,0 +1,38 @@
+// String topic
+const word = "facebook";
+console.log(word);
+
+// numbers or integers
+const score = 'Perfect';
+console.log(score);
+
+// Boolean
+const animelover = true;
+console.log (animelover);
+
+// Null
+const car = null;
+console.log(car);
+
+//Undefined
+let test;
+console.log(test);
+
+//Reference Types --  Object
+const hobbies = ['coding' , 'reading'];
+console.log(hobbies);
+
+//Object literal
+const address = {
+    city: 'Pasig',
+    street: 'Palatiw'
+};
+console.log(address);
+
+
+
+const today = new Date();
+today.setDate;
+console.log(today);
+console.log(typeof today);
+
