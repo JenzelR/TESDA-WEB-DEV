9642814b5f18856a13deb2fec400ac90fef94f31
Jenzel Rivera <78533457+JenzelR@users.noreply.github.com>
2021-02-24 19:00:22 +0800

    UC4 L02 assignments

---
+++ UC4-L02/index15/index15.php
@@ -0,0 +1,22 @@
+<?php 
+
+	//include 'samurai.php';
+
+	//require 'samurai.php';
+
+	//echo 'end of php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Tutorials</title>
+</head>
+<body>
+
+	<?php include('content.php'); ?>
+	<?php include('content.php'); ?>
+
+</body>
+</html>
---
+++ UC4-L02/index15/samurai.php
@@ -0,0 +1,7 @@
+<?php 
+
+	$samurai = ['shaun', 'the', 'sheep'];
+
+	echo $samurai[1] . '<br />';
+
+?> 
